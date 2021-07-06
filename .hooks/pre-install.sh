aa3985d7f40dfadac226ed40d1db63c6953bc2da
Djomangan Adama OUATTARA <douattara@gencovery.com>
2021-07-06 13:18:31 +0000

    [hook] wait for dlib build to finish

--- .hooks/pre-install.sh
+++ .hooks/pre-install.sh
@@ -6,3 +6,26 @@
 
 # Pre-installation script executed before server installation
 
+dlib_build_dir="/lab/.gws/externs/dlib-cpp/build"
+in_progress_file="$dlib_build_dir/IN_PROGRESS"
+
+n=1
+while [ -f "$in_progress_file" ] && [ $n -le 30 ]; do
+    echo "$n - Dlib build is in progress. Sleep 10 secs ..."
+    sleep 10
+    n=$(( $n + 1 ))
+done
+
+point="fba"
+gena_dir="/lab/gws/externs/gena-cpp"
+
+cd $gena_dir
+bazel build gena:${point}
+
+out_dir="/lab/gws/bricks/gena/bin/fba/"
+if [ ! -d "${out_dir}" ]; then
+    mkdir -p $out_dir
+fi
+
+cp "${gena_dir}/bazel-bin/gena/${point}" "${out_dir}/${point}"
+chmod a+x ${out_dir}/${point}
