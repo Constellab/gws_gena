06d58e9107d21b9567e0fc19e9be5ee0a272620c
Djomangan OUATTARA <douattara@gencovery.com>
2021-10-10 18:03:28 +0000

    (refactor)[cpp] remove gena-cpp dependencies

--- .hooks/pre-install.sh
+++ .hooks/pre-install.sh
@@ -5,30 +5,3 @@
 
 
 # Pre-installation script executed before server installation
-
-# ensure gaia is ready!
-gaia_dir="${PWD}/../../gws_gaia"
-if [ -d "${gaia_dir}" ]; then
-    python3 "${gaia_dir}/.hooks/pre-install.py"
-    bash "${gaia_dir}/.hooks/pre-install.sh"
-fi
-
-point="fba"
-gena_dir="/lab/.sys/lib/gena-cpp"
-if [ ! -d "${gena_dir}" ]; then
-    mkdir -p $gena_dir
-fi
-
-out_dir="${PWD}/../bin/fba"
-if [ ! -d "${out_dir}" ]; then
-    mkdir -p $out_dir
-fi
-
-# skip if already compiled
-if [ ! -f "${out_dir}/${point}" ]; then
-    cd $gena_dir
-    bazel build gena:${point}
-
-    cp "${gena_dir}/bazel-bin/gena/${point}" "${out_dir}/${point}"
-    chmod a+x ${out_dir}/${point}
-fi
