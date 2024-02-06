43044be94ef6836be696c1f646e38a3dacbac60e
Maëva <mbeugin@gencovery.com>
2024-02-06 15:58:29 +0000

    Add documentation

---
+++ tests/_cobra/test_convert_annotation.py
@@ -0,0 +1,29 @@
+from gws_gena.cobra.conversion_annotation.conversion_annotation import ConvertAnnotation
+from gws_core import (File,TaskRunner,Table,TableImporter)
+from gws_core import BaseTestCase
+
+
+class TestConvertAnnotation(BaseTestCase):
+    def test_convert_annotation(self):
+        ## Lactococcus lactis case ##
+        self.print(f"Test convert annotation : Lactococcus lactis")
+        #load genes:
+        model_input= File("/lab/user/bricks/gws_gena/tests/testdata/cobra/convert_annotation/iNF517.json")
+        #create the TaskRunner
+        runner_convert_annotation = TaskRunner(task_type=ConvertAnnotation,
+                                inputs={'input_model':  model_input},
+                                params = { "metabolites_id": "Other",
+                                        "metabolites_name": "Literal_name",
+                                        "reaction_id": "BiGG",
+                                        "reaction_name": "Other"})
+        #execute the TaskRunner
+        outputs_convert_annotation = runner_convert_annotation.run()
+
+        #check if we obtain the same output table
+        table = outputs_convert_annotation['output_results']
+        self.assertEqual([56],table.get_column_data("Rhea filled"))
+        self.assertEqual([754],table.get_column_data("Number reactions"))
+        self.assertEqual([375],table.get_column_data("EC number filled"))
+        self.assertEqual([650],table.get_column_data("Number metabolites"))
+        self.assertEqual([9],table.get_column_data("ChEBI filled"))
+
