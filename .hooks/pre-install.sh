f8eb7d43ff4def62c72ae22938da4f7f3b4ff1ee
Maëva <mbeugin@gencovery.com>
2024-02-09 14:10:20 +0000

    Add JAVA command

--- .hooks/pre-install.sh
+++ .hooks/pre-install.sh
@@ -1,3 +1,4 @@
 # File to install Java. 
 # Java is mandatory to use the task Twin Reducer because this Task uses the package efmtool which is a Java software.
+apt -y install default-jre
 apt -y install default-jdk
