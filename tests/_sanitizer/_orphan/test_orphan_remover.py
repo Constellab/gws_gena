4666dc65de306335f6e89010061e89c539a445db
Maëva <mbeugin@gencovery.com>
2024-02-13 08:39:34 +0000

    Add unitary test

---
+++ tests/_sanitizer/_orphan/test_orphan_remover.py
@@ -0,0 +1,29 @@
+
+import os
+
+from gws_biota import BaseTestCaseUsingFullBiotaDB
+from gws_core import ConfigParams, File, Settings, TaskRunner
+from gws_gena import OrphanRemover, NetworkImporter
+
+settings = Settings.get_instance()
+
+
+class TestOrphanRemover(BaseTestCaseUsingFullBiotaDB):
+
+    def test_orphan_remover(self):
+        data_dir = settings.get_variable("gws_gena:testdata_dir")
+
+        net = NetworkImporter.call(
+            File(path=os.path.join(data_dir, "toy_with_orphan/toy_with_orphan.json"))
+        )
+
+        runner_orphan_remover = TaskRunner(
+            params={},
+            inputs={"network": net},
+            task_type=OrphanRemover
+        )
+        outputs = runner_orphan_remover.run()
+        result = outputs["network"]
+
+        self.assertTrue("X_orphan_c" not in result.compounds)
+        self.assertTrue("X_fake_orphan_e" not in result.compounds)
