e1df673f187055ade9130558b5754edb8a040e03
mbeugin <mbeugin@gencovery.com>
2024-02-20 10:32:44 +0000

    Add unitary test for phenotype table

--- tests/_context/test_context_builder.py
+++ tests/_context/test_context_builder.py
@@ -2,8 +2,8 @@
 import os
 
 from gws_biota import BaseTestCaseUsingFullBiotaDB
-from gws_core import File, Settings, TaskRunner, TableImporter
-from gws_gena import ContextBuilder, FluxTableImporter, NetworkImporter
+from gws_core import File, Settings, TaskRunner
+from gws_gena import ContextBuilder, FluxTableImporter, NetworkImporter,PhenotypeTableImporter
 
 settings = Settings.get_instance()
 
@@ -18,13 +18,17 @@ class TestContext(BaseTestCaseUsingFullBiotaDB):
         file_path = os.path.join(data_dir, "ctx_data", "toy_ctx_data_1.csv")
         flux_data = FluxTableImporter.call(File(path=file_path), params={"delimiter": ","})
 
+        # pheno_table
+        file_path = os.path.join(data_dir, "phenotype_table_context.csv")
+        pheno_table = PhenotypeTableImporter.call(File(path=file_path), params={"delimiter": ","})
+
         # network
         file_path = os.path.join(data_dir, "toy.json")
         net = NetworkImporter.call(File(path=file_path), params={})
 
         # experiment
         tester = TaskRunner(
-            inputs={"network": net, "flux_table": flux_data},
+            inputs={"network": net, "flux_table": flux_data, 'pheno_table': pheno_table},
             task_type=ContextBuilder
         )
 
@@ -50,7 +54,36 @@ class TestContext(BaseTestCaseUsingFullBiotaDB):
                     ],
                 }
             ],
-            "compound_data": []
+            "compound_data": [
+                {
+                    "id": "met_X1_c",
+                    "name": "",
+                    "lower_bound": [0.0],
+                    "upper_bound": [1000.0],
+                    "target": [0.0],
+                    "confidence_score": [0.0],
+                    "variables": [
+                        {
+                            "reference_id": "X1_c",
+                            "coefficient": 1.0
+                        }
+                    ],
+                },
+                {
+                    "id": "met_X2_c",
+                    "name": "",
+                    "lower_bound": [-1],
+                    "upper_bound": [10.0],
+                    "target": [1.3],
+                    "confidence_score": [1.0],
+                    "variables": [
+                        {
+                            "reference_id": "X2_c",
+                            "coefficient": 1.0
+                        }
+                    ],
+                }
+
+            ]
         }
-        print(ctx.dumps())
         self.assertEqual(ctx.dumps(), expected_context)
