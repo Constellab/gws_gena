70526f0661e3059ea7f24c568f133ee602b19826
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-04-15 14:21:30 +0200

    (feat) remove licence text in files

--- src/gws_gena/mapper/bigg_mapper.py
+++ src/gws_gena/mapper/bigg_mapper.py
@@ -1,7 +1,3 @@
-# Gencovery software - All rights reserved
-# This software is the exclusive property of Gencovery SAS.
-# The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
-# About us: https://gencovery.com
 
 from gws_biota import Compound as BiotaCompound
 from gws_biota import Reaction as BiotaReaction
