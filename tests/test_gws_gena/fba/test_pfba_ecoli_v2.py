df78af91c15acb7a74a5f4d5db0e2527cb1a67ae
mbeugin <mbeugin@gencovery.com>
2024-07-12 12:44:37 +0000

    New tests with Tasks in version 2

---
+++ tests/test_gws_gena/fba/test_pfba_ecoli_v2.py
@@ -0,0 +1,96 @@
+import os
+
+import numpy
+import pandas
+from gws_biota import BaseTestCaseUsingFullBiotaDB
+from gws_core import File, IExperiment, Settings
+from gws_gena import ContextImporter
+from gws_gena.network_v2.network_importer_v2 import NetworkImporterV2
+from gws_gena.proto.fba_proto_v2 import FBAProtoV2
+
+settings = Settings.get_instance()
+
+
+class TestPFBA(BaseTestCaseUsingFullBiotaDB):
+
+    def test_pfba_ecoli(self):
+        data_dir = settings.get_variable("gws_gena:testdata_dir")
+
+        def run_fba(organism, solver="highs", relax_qssa=False):
+            experiment = IExperiment(FBAProtoV2)
+            proto = experiment.get_protocol()
+            organism_dir = os.path.join(data_dir, organism)
+
+            net = NetworkImporterV2.call(
+                File(os.path.join(organism_dir, f"{organism}.json")),
+                params = {"add_biomass" : True}
+            )
+            ctx = ContextImporter.call(File(
+                os.path.join(organism_dir, f"{organism}_context.json")
+            ))
+
+            proto.set_input("network", net)
+            proto.set_input("context", ctx)
+            fba = proto.get_process("fba")
+            fba.set_param('solver', solver)
+            fba.set_param('relax_qssa', relax_qssa)
+            fba.set_param('qssa_relaxation_strength', 1)
+            fba.set_param('parsimony_strength', 0.01)
+            if organism == 'ecoli':
+                # fba.set_param('fluxes_to_maximize', ["ecoli_BIOMASS_Ecoli_core_w_GAM:1.0"])
+                fba.set_param('biomass_optimization', "maximize")
+            else:
+                fba.set_param('fluxes_to_maximize', ["Biomass:1.0"])
+
+            experiment.run()
+
+            relax_dir = ""
+            if solver == "quad":
+                relax_dir = "relax" if relax_qssa else "no-relax"
+
+            # test results
+            result = proto.get_output("fba_result")
+
+            organism_result_dir = os.path.join(data_dir, 'pfba', organism)
+            result_dir = os.path.join(organism_result_dir, solver, relax_dir)
+            file_path = os.path.join(result_dir, "biomass_flux.csv")
+
+            # if not os.path.exists(result_dir):
+            #     os.makedirs(result_dir)
+            # with open(file_path, 'w', encoding="utf-8") as fp:
+            #     fp.write(biomass_flux.to_csv())
+
+            #biomass_flux = biomass_flux.to_numpy()
+            expected_biomass_flux = pandas.read_csv(file_path, index_col=0, header=0)
+            expected_biomass_flux = expected_biomass_flux.to_numpy()
+            # self.assertTrue(numpy.isclose(biomass_flux, expected_biomass_flux, rtol=1e-02).all())
+
+            fluxes = result.get_fluxes_dataframe()
+            print(fluxes)
+            print(fluxes.abs().sum())
+            sv = result.get_sv_dataframe()
+            th, p = result.compute_zero_flux_threshold()
+            print(f"sv_mean = {sv['value'].mean()}, sv_std = {sv['value'].std()}, sv_th={th}, sv_p = {p}")
+
+            # if not os.path.exists(result_dir):
+            #     os.makedirs(result_dir)
+            # file_path = os.path.join(result_dir, "flux.csv")
+            # with open(file_path, 'w', encoding="utf-8") as fp:
+            #     fp.write(fluxes.to_csv())
+            # file_path = os.path.join(result_dir, "sv.csv")
+            # with open(file_path, 'w', encoding="utf-8") as fp:
+            #     fp.write(sv.to_csv())
+
+            table = fluxes.to_numpy()
+            table = numpy.array(table, dtype=float)
+            file_path = os.path.join(result_dir, "flux.csv")
+            expected_table = pandas.read_csv(file_path, index_col=0, header=0)
+            expected_table = expected_table.to_numpy()
+            expected_table = numpy.array(expected_table, dtype=float)
+            self.assertTrue(numpy.isclose(table, expected_table, rtol=1e-01).all())
+
+        organism = "ecoli"
+
+        for relax in [False, True]:
+            # self.print(f"Test FBAProto: Medium- or large-size network ({organism} + quad)")
+            run_fba(organism=organism, solver="quad", relax_qssa=relax)
---
+++ tests/test_gws_gena/fva/test_fva_toy_v2.py
@@ -0,0 +1,94 @@
+import os
+
+import numpy
+import pandas
+from gws_core import File, Settings, TaskRunner, BaseTestCase
+from gws_gena import ContextImporter
+from gws_gena.fva.fva_result import FVAResult
+from gws_gena.network_v2.fva_v2 import FVAV2
+from gws_gena.network_v2.network_importer_v2 import NetworkImporterV2
+from gws_gena.network_v2.twin_v2 import TwinV2
+
+
+settings = Settings.get_instance()
+
+
+class TestFVA(BaseTestCase):
+
+    def test_toy_fva(self):
+        self.print("Test FVAProto: Small network")
+        testdata_dir = settings.get_variable("gws_gena:testdata_dir")
+        data_dir = os.path.join(testdata_dir, "toy")
+        organism_result_dir = os.path.join(testdata_dir, 'fva', "toy")
+
+        def run_fva(solver="highs", relax_qssa=False):
+            net = NetworkImporterV2.call(File(
+            path=os.path.join(data_dir, "toy.json")),
+            params={"add_biomass": True}
+            )
+            ctx = ContextImporter.call(
+                File(path=os.path.join(
+                    data_dir, ("toy_context.json"))
+            ))
+
+            # build the twin
+            twin = TwinV2()
+            twin.set_network(net)
+            twin.set_context(ctx)
+
+            task_runner = TaskRunner(FVAV2,
+                                 inputs={
+                                     "twin": twin
+                                 },
+                                 params={
+                                     "solver": solver,
+                                     "relax_qssa": relax_qssa
+                                 })
+
+            task_runner.run()
+
+            relax_dir = ""
+            if solver == "quad":
+                relax_dir = "relax" if relax_qssa else "no-relax"
+
+            # test results
+            result: FVAResult = task_runner.get_output("fva_result")
+            fluxes = result.get_fluxes_dataframe()
+
+            sv = result.get_sv_dataframe()
+
+            th, p = result.compute_zero_flux_threshold()
+            print(f"sv_mean = {sv['value'].mean()}, sv_std = {sv['value'].std()}, sv_th={th}, sv_p = {p}")
+
+            result_dir = os.path.join(organism_result_dir, solver, relax_dir)
+
+            # if not os.path.exists(result_dir):
+            #     os.makedirs(result_dir)
+            # file_path = os.path.join(result_dir, "sv.csv")
+            # with open(file_path, 'w', encoding="utf-8") as fp:
+            #     fp.write(sv.to_csv())
+            # file_path = os.path.join(result_dir, "flux.csv")
+            # with open(file_path, 'w', encoding="utf-8") as fp:
+            #     fp.write(fluxes.to_csv())
+
+            table = fluxes.to_numpy()
+            table = numpy.array(table, dtype=float)
+
+            file_path = os.path.join(result_dir, "flux.csv")
+            expected_table = pandas.read_csv(file_path, index_col=0, header=0)
+            expected_table = expected_table.to_numpy()
+            expected_table = numpy.array(expected_table, dtype=float)
+
+            self.assertTrue(numpy.isclose(table, expected_table, rtol=1e-01).all())
+
+            # # export annotated network
+            # annotated_twin = proto.get_output("twin")
+            # net = list(annotated_twin.networks.values())[0]
+            # with open(os.path.join(result_dir, './toy_fva_dump.json'), 'w', encoding="utf-8") as fp:
+            #     data = net.dumps()
+            #     json.dump(data, fp, indent=4)
+
+        run_fva(solver="highs")
+
+        for relax_qssa in [True, False]:
+            run_fva(solver="quad", relax_qssa=relax_qssa)
