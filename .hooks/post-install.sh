8370fe59cb70d4d584876b34c8aec64663362cdc
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-09-05 12:53:41 +0000

    (feat) disable install jdk

--- .hooks/post-install.sh
+++ .hooks/post-install.sh
@@ -1,7 +1,7 @@
 # File to install Java. 
 # Java is mandatory to use the task Twin Reducer because this Task uses the package efmtool which is a Java software.
-echo "[GWS_GENA] Installing java jdk"
-export DEBIAN_FRONTEND=noninteractive
-apt update
-apt -y install default-jdk
-echo "[GWS_GENA] Java jdk successfully installed"
+# echo "[GWS_GENA] Installing java jdk"
+# export DEBIAN_FRONTEND=noninteractive
+# apt update
+# apt -y install default-jdk
+# echo "[GWS_GENA] Java jdk successfully installed"
